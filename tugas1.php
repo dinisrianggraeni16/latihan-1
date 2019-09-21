375060286be9c4d6e8c7bbcc889535492afd11e0
dinisrianggraeni16 <55383964+dinisrianggraeni16@users.noreply.github.com>
2019-09-21 12:57:20 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,57 @@
+<?php
+
+$x = 10;
+$y = 20;
+
+echo "luas persegi panjang P x L";
+echo "<br>";
+echo "panjang =". $x;
+echo "<br>";
+echo "luas =" . $y;
+echo "<br>";
+echo "luas persegi panjang adalah : " . $x*$y;
+
+
+echo "<br>";
+echo "<br>";
+
+$x = 15;
+$y = 10;
+
+echo "luas jajar genjang a x t";
+echo "<br>";
+echo "alas=".$x;
+echo "<br>";
+echo "tinggi=".$y;
+echo "<br>";
+echo "luas jajar genjang adalah:". $x*$y;
+
+
+echo "<br>";
+echo "<br>";
+
+$x = 22/7;
+$y = 28;
+
+echo "luas lingkaran π x r x r";
+echo "<br>";
+echo "phi=".$x;
+echo "<br>";
+echo "jari-jari=".$y;
+echo "<br>";
+echo "luas persegi panjang adalah:".$x*$y*$y;
+
+echo "<br>";
+echo "<br>";
+
+$x = 7;
+$y = 14;
+
+echo "luas segitiga 1/2 x a x t";
+echo "<br>";
+echo "alas=". $x;
+echo "<br>";
+echo "tinggi=".$y;
+echo "<br>";
+echo "luas segitiga adalah:". 1/2*$x*$y;
+?>
